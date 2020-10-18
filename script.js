a145a32878a1625a935687328eb003eb80262147
Eduard Korkuts <Zealottpkr@gmail.com>
2020-10-18 03:10:57 +0300

    refactor: changed the text in the tooltip

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function createHint(inputName) {
     switch (inputName) {
         case "username":
             hint.innerHTML =
-                "<p> не более 20 латинских символов, не должно содержать пробелы и спецсимволы</p>";
+                "<p> от 1 до 20 латинских символов, не должно содержать пробелы и спецсимволы</p>";
 
             hint.id = "username";
             break;
